a7cf172844dc998d90504c3b937d82db90e353da
zhangtan <ztreal@gmail.com>
2012-07-31 15:31:43 +0800

    zt的例子工程
    初稿，只包含spring和mongo

--- zt-example-web/src/test/dao/MongoTest.java
+++ zt-example-web/src/test/dao/MongoTest.java
@@ -1,14 +1,19 @@
 package dao;
 
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 import com.mongodb.Mongo;
+import com.mongodb.WriteResult;
 import com.zt.todo.log.UserLog;
 import org.junit.Test;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
-
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.data.mongodb.core.query.Update;
 import javax.annotation.Resource;
 import java.net.UnknownHostException;
+import java.util.List;
 
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
@@ -20,17 +25,43 @@ import static org.springframework.data.mongodb.core.query.Criteria.where;
  */
 public class MongoTest extends BaseTestCase {
 
-     private  MongoTemplate mongoOps;
+    private MongoTemplate mongoOps;
 
     @Test
     public void insertLog() throws UnknownHostException {
+        UserLog userLog = new UserLog("zt", "吃饭");
 
-     mongoOps.insert(new UserLog("zt", "我今天吃饭了"));
+        mongoOps.insert(new UserLog("zt", "我今天吃饭了"));
 
-     log.info(mongoOps.findOne(new Query(where("operater").is("zt")), UserLog.class));
 
-     mongoOps.dropCollection("person");
+
+//     mongoOps.dropCollection("person");
+
+
+        //save
+        mongoOps.save(userLog,"userLog");
+
+        //find
+        UserLog savedLog =  mongoOps.findOne(new Query(where("operater").is("zt")), UserLog.class);
+
+        System.out.println("savedLog : " + savedLog);
+
+        //update
+        WriteResult updateResult =  mongoOps.updateFirst(new Query(where("operater").is("zt")),
+                Update.update("operater", "qs"), UserLog.class);
+
+
+        System.out.println("updateResult : " + updateResult.getN());
+
+        //delete
+        mongoOps.remove(new Query(where("operater").is("zt")),"userLog");
+
+        //List
+        DBCollection dBCollection = mongoOps.getCollection("userLog");
+
+        System.out.println("Number of user = " + dBCollection.getCount());
     }
+
 
     public MongoTemplate getMongoOps() {
         return mongoOps;
