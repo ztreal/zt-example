298273c2d57fa02e31e1a4d3fb468eadc3e1ec9d
zhangtan <ztreal@gmail.com>
2013-01-25 23:36:27 +0800

    test

---
+++ zt-exampleman-web/src/test/java/DynamicProxyTest2.java
@@ -0,0 +1,15 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhangtan
+ * Date: 12-8-1
+ * Time: 下午11:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class DynamicProxyTest2 {
+//    interface IHello{
+//        void sayhello();
+//    }
+//    static class Hello implements Ihello{
+//        @Override
+//    }
+}
